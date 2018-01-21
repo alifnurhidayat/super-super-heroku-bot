ee8340843e3fb944d0d2cf79e82337caad14a3cb
alifnurhidayat <35630091+alifnurhidayat@users.noreply.github.com>
2018-01-21 08:12:36 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NDA0MjUyOTkyNjE5OTM3Nzky.DUTbnw.EXrDdYKfv3P6CVOzdiLbozTo7VY);
